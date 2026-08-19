9a4ba5e096a81f13d8bc0bbed5a54e43c41066f5
NHKizChilling <nhoangkhai195@gmail.com>
2026-08-19 12:31:01 +0700

    chore: bootstrap development environment

---
+++ services/workflow-service/src/test/java/com/weav/workflow/TestWorkflowServiceApplication.java
@@ -0,0 +1,11 @@
+package com.weav.workflow;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestWorkflowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(WorkflowServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
